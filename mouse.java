5d0898f5bb5ffd4d8b2fe1c5f87f3b417facdab2
shaddowlinkk <nathan.vanbrunt@gmail.com>
2018-05-27 11:41:54 -0400

    use key listener to get event for global usage not when ky is prpresed you can get mouse pos ther ever it is on the screen as long as the app is selected

--- mouse.java
+++ mouse.java
@@ -4,25 +4,33 @@ import java.awt.*;
 import java.util.concurrent.TimeUnit;
 import java.awt.MouseInfo;
 public class mouse extends JFrame{
-	private JLabel pos;
-	private double posx;
-	private double posy;
-	public  mouse () {
-		setSize(150,300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
+    private JLabel pos;
+    private double posx;
+    private double posy;
+    public  mouse () {
+        setSize(150,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
 
-		addMouseListener(new MouseAdapter(){
-			public void mousePressed(MouseEvent e){
-				posx = MouseInfo.getPointerInfo().getLocation().getX();
-                                posy = MouseInfo.getPointerInfo().getLocation().getY();
-				System.out.println("you clicked posX:"+posx+" posY:"+posy);
-			}
-		});
+        addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
 
-	}
-	public static void main (String args[]){
-	new mouse().setVisible(true);
-	}
+            }
+
+            public void keyPressed(KeyEvent e) {
+                posx = MouseInfo.getPointerInfo().getLocation().getX();
+                posy = MouseInfo.getPointerInfo().getLocation().getY();
+                System.out.println("you clicked posX:"+posx+" posY:"+posy);
+            }
+
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        pack();
+    }
+    public static void main (String args[]){
+        new mouse().setVisible(true);
+    }
 }
 
